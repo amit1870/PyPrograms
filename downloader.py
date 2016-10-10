f158513efefb7ffb4fb7b8ddf145f1b62a31debf
Amit Patel <patelamit1870@gmail.com>
2016-10-10 12:19:29 +0530

    Changed youtube download way
    Changed

--- downloader.py
+++ downloader.py
@@ -3,28 +3,22 @@ from urllib import urlopen
 from os import listdir
 from os.path import isfile, join
 
-Music = "/home/amit/Videos"
-
-def youtube_dl(file):
-	# lines = []
-	with open(file) as f:
-		lines = f.read().splitlines()
-		if lines != []:
-			lines = map(lambda x : x.strip(), lines)
-			if "" in lines:
-				lines = lines.remove("")
-			f.close()
-			
-			if len(lines) > 0:
-				for line in lines:
-					# download video
-					os.chdir(Music)
-					command = "youtube-dl -f 18 -q https://www.youtube.com/watch?v=%s" %line
-					print command
-					os.system(command)
+def download_dl(vcode):
+	cwd = os.curdir
+	os.chdir(cwd)
+	if os.environ['OS'] == "WINDOWS_NT":
+		command = "youtube-dl.exe -f 18 https://www.youtube.com/watch?v=%s" % vcode
+	else:
+		command = "youtube-dl -f 18 https://www.youtube.com/watch?v=%s" % vcode
+	print command
+	os.system(command)
 
 
-status = urlopen("http://www.youtube.com").getcode()
 
-if status == 200:
-	youtube_dl("urls.txt")
+
+if __name__ == "__main__":
+	status = urlopen("http://www.youtube.com").getcode()
+	if status == 200:
+		vcodes = ['tTFr78q0lXM',]
+		for vcode in vcodes:
+			download_dl(vcode)
