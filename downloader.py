b137b94e09835318c998221231650699f6a25c9d
amit1870 <patelamit1870@gmail.com>
2016-10-26 21:43:20 +0530

    changing downloader

--- downloader.py
+++ downloader.py
@@ -6,19 +6,14 @@ from os.path import isfile, join
 def download_dl(vcode):
 	cwd = os.curdir
 	os.chdir(cwd)
-	if os.environ['OS'] == "WINDOWS_NT":
-		command = "youtube-dl.exe -f 18 https://www.youtube.com/watch?v=%s" % vcode
-	else:
-		command = "youtube-dl -f 18 https://www.youtube.com/watch?v=%s" % vcode
+	command = "youtube-dl -f 18 https://www.youtube.com/watch?v=%s" % vcode
 	print command
 	os.system(command)
-
-
 
 
 if __name__ == "__main__":
 	status = urlopen("http://www.youtube.com").getcode()
 	if status == 200:
-		vcodes = ['tTFr78q0lXM',]
+		vcodes = ['6ZW6UqJOVgU','IcdRABIYv9E','qRErR62SAH0','BJmkS7azuGU']
 		for vcode in vcodes:
 			download_dl(vcode)
